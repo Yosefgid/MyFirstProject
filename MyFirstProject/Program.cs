02e17e4baac2f42d872187d9b716c0d802d53922
Yosefgid <jossykasshun@yahoo.com>
2026-03-10 10:55:25 +0000

    Inital commits to set everything up

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+            Console.WriteLine(args[2]);
+            //Console.WriteLine("Hello, my name is: Yosef");
+            //Console.WriteLine("My age is: " + 28);
+            Console.WriteLine("My favourite film is: Jumanji!");
+            string name = "Imad";
+            Console.WriteLine("My name is: " + name);
+            int age = 36;
+            Console.WriteLine("My age is: " + age);
+        }
+    }
+}
